cc7c6bd8e3fa67f63c471537bcd8dc0825f311f2
jwaltrip <jake.waltrip@gmail.com>
2018-08-28 16:28:28 -0500

    initial static structure built

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* HEADER + STATS*/}
         <div className="header">
           <table className="stats">
             <tbody>
@@ -22,6 +23,39 @@ class App extends Component {
             <span className="date-big">28</span>
             <span className="date-small"> Aug</span>
           </div>
+        </div>
+        {/* TASKS CONTAINER */}
+        <div className="tasks">
+          {/* INDIVIDUAL TASK */}
+          <div className="task">
+            <div className="radio">
+              <div className="radio-inactive"> </div>
+            </div>
+            <div className="task-name">
+              Task 1
+            </div>
+            <div className="remove-btn">
+              <a className="remove-task-btn">✖</a>
+            </div>
+          </div>
+          {/* INDIVIDUAL TASK */}
+          <div className="task">
+            <div className="radio">
+              <div className="radio-inactive"> </div>
+            </div>
+            <div className="task-name">
+              Task 2
+            </div>
+            <div className="remove-btn">
+              <a className="remove-task-btn">✖</a>
+            </div>
+          </div>
+        </div>
+
+        {/* FOOTER FORM */}
+        <div className="footer-form">
+          <input type="text" value="" />
+          <input type="submit" value="ADD" />
         </div>
       </div>
     );
