9e088e1617b28e91e239f7b00aeb08f8fb260d71
jwaltrip <jake.waltrip@gmail.com>
2018-08-30 13:23:48 -0500

    header date time state is working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react';
 import Stats from "./Stats";
 
-const Header = (props) => {
-  return (
-    <div className="header">
-      <Stats/>
-      <div className="date">
-        <span className="date-big">30</span>
-        <span className="date-small"> Aug</span>
-        <span className="time"> @ 12:17 pm</span>
+// const Header = (props) => {
+class Header extends Component {
+  state = {
+    dayOfMonth: "",
+    month: "",
+    currentTime: ""
+  };
+
+  pollInterval = null;
+
+  componentDidMount() {
+    // get current DateTime every 1 seconds
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.getCurrentDateTime, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  getCurrentDateTime = () => {
+    // make a copy of current state using spread operator
+    const newState = {...this.state};
+    // arr of months abrv
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    // get current datetime in seconds
+    const today = new Date();
+    // get dayOfMonth, month, and time
+    const currDayOfMonth = today.getDate();
+    const currMonth = months[today.getMonth()];
+    let hour = today.getHours();
+    let min = today.getMinutes();
+
+    // convert time to 12 hour format w am/pm
+    let amPm = 'am';
+    if (hour >= 12) {
+      if (hour > 12) hour -= 12;
+      amPm = 'pm';
+    }
+
+    // concat current time + am/pm
+    const currTime = hour + ':' + min + ' ' + amPm;
+
+    // set new time in state obj
+    newState.dayOfMonth = currDayOfMonth;
+    newState.month = currMonth;
+    newState.currentTime = currTime;
+    // set state 
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <Stats/>
+        <div className="date">
+          <span className="date-big">{this.state.dayOfMonth}</span>
+          <span className="date-small"> {this.state.month}</span>
+          <span className="time"> @ {this.state.currentTime}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
