9fef5aff201b4c8f3c0fdb109abe4528e2a9e29f
jwaltrip <jake.waltrip@gmail.com>
2018-08-31 14:22:24 -0500

    initial refactor of multiline task name working

--- src/components/Task.js
+++ src/components/Task.js
@@ -13,11 +13,9 @@ class Task extends Component {
       <div className={"task" + isCompleted}>
         <div className="toggle-click" onClick={this.props.handleTaskToggle}>
           <div className={"radio" + isCompleted}> </div>
-          {/*<div className="task-name-container">*/}
             <div className={"task-name" + isCompleted}>
               {this.props.taskName}
             </div>
-          {/*</div>*/}
         </div>
         <div className="remove-btn"
              onClick={()=> {this.props.handleTaskRemove(this.props.id)}}>
