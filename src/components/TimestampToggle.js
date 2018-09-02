258d09d8741482012f574145f0bc8054ed941f62
jwaltrip <jake.waltrip@gmail.com>
2018-09-02 16:46:10 -0500

    added clock icon with working keyframe animation

--- src/components/TimestampToggle.js
+++ src/components/TimestampToggle.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const TimestampToggle = (props) => {
   return (
-    <div className="timestamp-toggle">
-      <i className="far fa-clock fa-lg timestamp-toggle-btn"> </i>
-      <div className="timestamp-toggle-text">toggle timestamps</div>
+    <div>
+      <div className="timestamp-toggle">
+        <div className="timestamp-toggle-text">toggle timestamps</div>
+        <i className="far fa-clock fa-lg timestamp-toggle-btn"> </i>
+      </div>
     </div>
   );
 };
